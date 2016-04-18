fbbfa30f7c81afb848c4ecbac22f996194f5f7a8
rafej <rafe.s@hotmail.com>
2016-04-19 08:54:23 +1000

    Added more of a storyline

--- game.py
+++ game.py
@@ -62,7 +62,26 @@ if getUp == "y":
 									print "You commented: Thanks Guys!"
 								if comment1 == 2:
 									print "You commented: Stop being sarcastic, you don't give two shits about me"
-
+					if phone2 == 2:
+						print "Open Messenger"
+						print "Loading"
+						print "4 New Messages"
+						print "Bella said: Babe why the hell did you hang up! (1)"
+						print "Ali said: What you doing tomorrow after school?(2)"
+						print "James said: Yo dude, what was that maths homework?(3)"
+						print "In 'The Clan': Bella said: Anyone heard from him?(4)"
+						reply1 = int(raw_input("Reply to 1,2,3 or 4: (1-4): "))
+						if reply1 == 1:
+							print "Reply with either:"
+							print "Babe, I'm so sorry. Please forgive me! :) (1)", "\n", "or", "\n", "Well, I'm sorry I'm a nuicence.(2)"
+							messege1 = int(raw_input("Reply with (1) or (2) (1 or 2): "))
+							if messege1 == 1:
+								print "You replied with: ", "\n","Babe, I'm so sorry. Please forgive me! :)"
+								print "You wait 2 mins and 3 seconds for a response"
+								print "Ding, ding"
+								print "(Messenger) (1) Bella messaged you"
+								print "Bella said: Mate, it doesn't work like that!"
+								reply2 = str(raw_input("Do you want to reply?(y/n): "))
 
 		if door1 == "y":
 			print "You walk downstairs"
@@ -102,7 +121,36 @@ if getUp == "n":
 				print "Babe, I'm so sorry. Please forgive me! :) (1)", "\n", "or", "\n", "Well, I'm sorry I'm a nuicence.(2)"
 				messege1 = int(raw_input("Reply with (1) or (2) (1 or 2): "))
 				if messege1 == 1:
-					print
+					print "You replied with: ", "\n","Babe, I'm so sorry. Please forgive me! :)"
+					print "You wait 2 mins and 3 seconds for a response"
+					print "Ding, ding"
+					print "(Messenger) (1) Bella messaged you"
+					print "Bella said: Mate, it doesn't work like that!"
+					reply2 = str(raw_input("Do you want to reply?(y/n): "))
+					if reply2 == "n":
+						print "You put down your phone and stare into the light peering out of your window momentarily"
+						getUp = str(raw_input("Do you want to get out of bed?:(y/n) "))
+						if getUp == "y":
+							print "You got up"
+							print "Slowly but surely, your body aches out of bed"
+							print "You drudge to your wardrobe."
+							drobe1 = str(raw_input("Do you want to put on your school uniform? (y/n): "))
+							if drobe1 == "n":
+								print "You think about putting on some clothes but then you are like, hmm maybe no"
+								stairs1 = str(raw_input("Do you want to go downstairs?(y/n): "))
+								if stairs1 == "y":
+									print "You walk downstairs in your pyjamas"
+									print "Clunk, clunk clunk"
+									print "The sun billows through the windows in streams of light"
+									eat2 = str(raw_input("Do you want to have breakfast?(y/n): "))
+									if eat2 == "y":
+										eat1 = str(raw_input("Do you want to eat cereal or toast? (c/t): "))
+										if eat1 == "c":
+											cereal1 = str(raw_input("Nutri-Grain or Weet Bix? (n/w): "))
+											if cereal1 == "n":
+												print "You get the Nutri-Grain out of the cupboard slowly"
+												print "Eventually pouring milk into the bowl of cereal, you eat your breakfast tediously."
+
 
 
 		if phone2 == 1:
@@ -118,7 +166,7 @@ if getUp == "n":
 					print "You commented: Thanks Guys!"
 				if comment1 == 2:
 					print "You commented: Stop being sarcastic, you don't give two shits about me"
-				elif comment1 >= 3:
+				while comment1 >= 3:
 					print "Invalid response"
 					comment1 = int(raw_input("Reply with - Thanks Guys! (1) or Stop being sarcastic, you don't give two shits about me (2): "))
 					if comment1 == 1:
